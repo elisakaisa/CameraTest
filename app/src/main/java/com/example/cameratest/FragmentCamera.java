bbf45c7eedab771d476ae21588a48c42da27e8dc
elisakaisa <elisa.k.perini@gmail.com>
2022-12-02 16:09:40 +0100

    getting some data from ML kit in debugger

--- app/src/main/java/com/example/cameratest/FragmentCamera.java
+++ app/src/main/java/com/example/cameratest/FragmentCamera.java
@@ -2,11 +2,18 @@ package com.example.cameratest;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -15,12 +22,21 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.util.Log;
+import android.util.Size;
 import android.view.LayoutInflater;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+import com.google.android.gms.tasks.Task;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.pose.Pose;
+import com.google.mlkit.vision.pose.PoseDetection;
+import com.google.mlkit.vision.pose.PoseDetector;
+import com.google.mlkit.vision.pose.PoseLandmark;
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions;
 
 import java.util.concurrent.ExecutionException;
 
@@ -37,6 +53,7 @@ public class FragmentCamera extends Fragment {
     PreviewView pv;
     private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
     private static final int CAMERA_REQUEST_CODE = 10;
+    PoseDetector poseDetector;
 
 
     public FragmentCamera() {
@@ -61,19 +78,17 @@ public class FragmentCamera extends Fragment {
 
         // camera provider
         pv = view.findViewById(R.id.viewFinder);
-
-
         if (hasCameraPermission()) {
             cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext());
         } else {
             requestPermission();
         }
 
-
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
                 bindPreview(cameraProvider);
+                poseDetection();
             } catch (ExecutionException | InterruptedException e) {
                 // No errors need to be handled for this Future.
                 // This should never be reached.
@@ -83,9 +98,44 @@ public class FragmentCamera extends Fragment {
         return view;
     }
 
-    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
-        Preview preview = new Preview.Builder()
-                .build();
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        ImageAnalysis imageAnalysis =
+                new ImageAnalysis.Builder()
+                        .setTargetResolution(new Size(1280, 720))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        //.setImageQueueDepth(1)
+                        .build();
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(requireContext()), new ImageAnalysis.Analyzer() {
+            @androidx.camera.core.ExperimentalGetImage
+            @Override
+            public void analyze(@NonNull ImageProxy imageProxy) {
+                Image mediaImage = imageProxy.getImage();
+                if (mediaImage != null) {
+                    InputImage image =
+                            InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+                    // Pass image to an ML Kit Vision API
+                    Task<Pose> result =
+                            poseDetector.process(image)
+                                    .addOnSuccessListener(
+                                            pose -> {
+                                                //Log.i("analyze", "success " + pose);
+                                                processPose(pose);
+
+                                                // need to be closed
+                                                mediaImage.close();
+                                                imageProxy.close();
+                                            })
+                                    .addOnFailureListener(
+                                            e -> {
+                                                // Task failed with an exception
+                                                Log.e("analyze", "task failed: " + e);
+                                            });
+                }
+            }
+        });
+
+
+        Preview preview = new Preview.Builder().build();
 
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
@@ -93,7 +143,18 @@ public class FragmentCamera extends Fragment {
 
         preview.setSurfaceProvider(pv.getSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageAnalysis, preview);
+    }
+
+    private void poseDetection() {
+        // pose detection
+        // Accurate pose detector on static images, when depending on the pose-detection-accurate sdk
+        AccuratePoseDetectorOptions options =
+                new AccuratePoseDetectorOptions.Builder()
+                        .setDetectorMode(AccuratePoseDetectorOptions.STREAM_MODE)
+                        .build();
+        poseDetector = PoseDetection.getClient(options);
+
     }
 
     private boolean hasCameraPermission() {
@@ -110,4 +171,59 @@ public class FragmentCamera extends Fragment {
                 CAMERA_REQUEST_CODE
         );
     }
+
+
+
+    // TODO: encapsulate this
+    private void processPose(Pose pose) {
+        PoseLandmark nose = pose.getPoseLandmark(PoseLandmark.NOSE);
+        PoseLandmark lefyEyeInner = pose.getPoseLandmark(PoseLandmark.LEFT_EYE_INNER);
+        PoseLandmark lefyEye = pose.getPoseLandmark(PoseLandmark.LEFT_EYE);
+        PoseLandmark leftEyeOuter = pose.getPoseLandmark(PoseLandmark.LEFT_EYE_OUTER);
+        PoseLandmark rightEyeInner = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE_INNER);
+        PoseLandmark rightEye = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE);
+        PoseLandmark rightEyeOuter = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE_OUTER);
+        PoseLandmark leftEar = pose.getPoseLandmark(PoseLandmark.LEFT_EAR);
+        PoseLandmark rightEar = pose.getPoseLandmark(PoseLandmark.RIGHT_EAR);
+        PoseLandmark leftMouth = pose.getPoseLandmark(PoseLandmark.LEFT_MOUTH);
+        PoseLandmark rightMouth = pose.getPoseLandmark(PoseLandmark.RIGHT_MOUTH);
+
+        PoseLandmark leftShoulder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER);
+        if (leftShoulder != null) {
+            String s = String.valueOf(leftShoulder.getPosition().x);
+            Log.i("pose-coordinates", "shoulder x " + s);
+        }
+        PoseLandmark rightShoulder = pose.getPoseLandmark(PoseLandmark.RIGHT_SHOULDER);
+        PoseLandmark leftElbow = pose.getPoseLandmark(PoseLandmark.LEFT_ELBOW);
+        PoseLandmark rightElbow = pose.getPoseLandmark(PoseLandmark.RIGHT_ELBOW);
+        PoseLandmark leftWrist = pose.getPoseLandmark(PoseLandmark.LEFT_WRIST);
+        PoseLandmark rightWrist = pose.getPoseLandmark(PoseLandmark.RIGHT_WRIST);
+        PoseLandmark leftHip = pose.getPoseLandmark(PoseLandmark.LEFT_HIP);
+        if (leftHip != null) {
+            String s = String.valueOf(leftHip.getPosition().x);
+            Log.i("pose-coordinates", "hip x " + s);
+        }
+        PoseLandmark rightHip = pose.getPoseLandmark(PoseLandmark.RIGHT_HIP);
+        PoseLandmark leftKnee = pose.getPoseLandmark(PoseLandmark.LEFT_KNEE);
+        PoseLandmark rightKnee = pose.getPoseLandmark(PoseLandmark.RIGHT_KNEE);
+        if (rightKnee != null) {
+            String s = String.valueOf(rightKnee.getPosition().x);
+            Log.i("pose-coordinates", "r knee x " + s);
+        }
+        PoseLandmark leftAnkle = pose.getPoseLandmark(PoseLandmark.LEFT_ANKLE);
+        PoseLandmark rightAnkle = pose.getPoseLandmark(PoseLandmark.RIGHT_ANKLE);
+
+        PoseLandmark leftPinky = pose.getPoseLandmark(PoseLandmark.LEFT_PINKY);
+        PoseLandmark rightPinky = pose.getPoseLandmark(PoseLandmark.RIGHT_PINKY);
+        PoseLandmark leftIndex = pose.getPoseLandmark(PoseLandmark.LEFT_INDEX);
+        PoseLandmark rightIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_INDEX);
+        PoseLandmark leftThumb = pose.getPoseLandmark(PoseLandmark.LEFT_THUMB);
+        PoseLandmark rightThumb = pose.getPoseLandmark(PoseLandmark.RIGHT_THUMB);
+        PoseLandmark leftHeel = pose.getPoseLandmark(PoseLandmark.LEFT_HEEL);
+        PoseLandmark rightHeel = pose.getPoseLandmark(PoseLandmark.RIGHT_HEEL);
+        PoseLandmark leftFootIndex = pose.getPoseLandmark(PoseLandmark.LEFT_FOOT_INDEX);
+        PoseLandmark rightFootIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_FOOT_INDEX);
+    }
+
+
 }
